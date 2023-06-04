6e4253cbaf71da0967ef2b406ee5552e5a4b64f8
mohammed3599 <kataloni1999@gmail.com>
2023-06-04 12:04:19 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 4; i++)
+            {
+                    Console.Write("*");
+            }
+           
+            Console.WriteLine();
+            
+            for(int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("*   *");
+            }
+           
+            for(int i = 0; i <= 4; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
